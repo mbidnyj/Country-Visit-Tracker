b069734de3c336d3eefcf7d6ef5dd4dda65d5f76
mbidnyj <mbidnyj@gmail.com>
2024-08-07 22:28:50 +0200

    using init method of redisClient in seed file

--- www/seed.js
+++ www/seed.js
@@ -1,22 +1,32 @@
-const { redisClient } = require("./redisClient");
+const { redisClient, init } = require("./redisClient");
 
-redisClient.on("ready", async () => {
-    console.log("Connected to Redis");
+const seedData = async () => {
+    try {
+        await redisClient.incrBy("us", 1200);
+        await redisClient.incrBy("ua", 450);
+        await redisClient.incrBy("it", 300);
+        await redisClient.incrBy("fr", 150);
+        await redisClient.incrBy("de", 80);
+        await redisClient.incrBy("es", 35);
+        await redisClient.incrBy("ca", 15);
+        await redisClient.incrBy("br", 5);
+        await redisClient.incrBy("au", 0);
 
-    await redisClient.incrBy("us", 1200);
-    await redisClient.incrBy("ua", 450);
-    await redisClient.incrBy("it", 300);
-    await redisClient.incrBy("fr", 150);
-    await redisClient.incrBy("de", 80);
-    await redisClient.incrBy("es", 35);
-    await redisClient.incrBy("ca", 15);
-    await redisClient.incrBy("br", 5);
-    await redisClient.incrBy("au", 0);
+        console.log("Data has been seeded");
+    } catch (err) {
+        console.error("Error seeding data:", err);
+    } finally {
+        redisClient.quit();
+    }
+};
 
-    console.log("Data has been seeded");
-    redisClient.quit();
-});
+const main = async () => {
+    try {
+        await init();
+        await seedData();
+    } catch (err) {
+        console.error("Failed to initialize Redis and seed data:", err);
+    }
+};
 
-redisClient.on("error", (err) => {
-    console.error("Redis error:", err);
-});
+main();
